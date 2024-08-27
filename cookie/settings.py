7e00192f23de6861265b0bb087828f2229c7f1b3
zubbby <nwokenzube54@gmail.com>
2024-08-27 18:39:42 +0100

    Update settings.py

--- cookie/settings.py
+++ cookie/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-0hkxfv97%3ozxl@o8x!r1g-005uur9*q#&reshx_cvk-f$*rmh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["doc-oneinbox.onrender.com"]
 
 
 # Application definition
